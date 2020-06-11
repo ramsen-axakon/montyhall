e4a4ae3a677f4aa56078538b421530518fbb7a30
ramsenaziz <ramsen.aziz.privat@gmail.com>
2020-06-11 21:42:14 +0200

    Add react devtools

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export default configureStore({
+  devTools: true,
   reducer: {
     counter: counterReducer,
   },
