558991145a8b11545fc8b77b0cf4f4208f938251
ramsenaziz <ramsen.aziz.privat@gmail.com>
2020-06-13 07:53:40 +0200

    Updates

---
+++ precache-manifest.dbdedbaa7b356d8ad6293a3fa3d2904b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21449ed70ae0457021481c153f56aad",
+    "url": "/montyhall/index.html"
+  },
+  {
+    "revision": "3b44a0ec11b3deb31ed4",
+    "url": "/montyhall/static/css/main.1569f39c.chunk.css"
+  },
+  {
+    "revision": "262da477fff3c319ea89",
+    "url": "/montyhall/static/js/2.0067fbb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/montyhall/static/js/2.0067fbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b44a0ec11b3deb31ed4",
+    "url": "/montyhall/static/js/main.e5b1974f.chunk.js"
+  },
+  {
+    "revision": "ae3570f76c2fcb42d709",
+    "url": "/montyhall/static/js/runtime-main.718674c3.js"
+  },
+  {
+    "revision": "8d2426330c4f9f327a8dfe11f350a7b1",
+    "url": "/montyhall/static/media/money.8d242633.svg"
+  },
+  {
+    "revision": "61b92ce6595ddb5b71bce61d1a4fba76",
+    "url": "/montyhall/static/media/monty_1.61b92ce6.jpg"
+  },
+  {
+    "revision": "ed8b4911bb4e1aed3653e4a9ccc3b4f4",
+    "url": "/montyhall/static/media/monty_2.ed8b4911.jpg"
+  },
+  {
+    "revision": "64641eb21353c81e30b4ef2a9de2da9b",
+    "url": "/montyhall/static/media/monty_3.64641eb2.jpg"
+  },
+  {
+    "revision": "5f4d136decd29f4c6ec4a8059a9ca59d",
+    "url": "/montyhall/static/media/monty_4.5f4d136d.jpeg"
+  },
+  {
+    "revision": "de75b8d5a54c4e3e9e427d3af6510e44",
+    "url": "/montyhall/static/media/monty_5.de75b8d5.jpg"
+  },
+  {
+    "revision": "726d9044fbf5611f310f5d21d24818e5",
+    "url": "/montyhall/static/media/monty_6.726d9044.jpg"
+  }
+]);
