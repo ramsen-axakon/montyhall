d49a70b0583ff26c983d257d018f6e3aa6d0508e
ramsenaziz <ramsen.aziz.privat@gmail.com>
2020-06-13 07:24:56 +0200

    Add random background

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { getScore } from "./selectors";
+import montyImages from "./utils/montyImages";
+import { getRandomMonty } from "./utils/getRandomHelper";
+import Message from "./Message";
+import Boxes from "./Boxes";
+import Score from "./Score";
+
+import "./App.css";
 
 function App() {
+  const score = useSelector(({ app }) => getScore(app));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div
+      className="app"
+      style={{
+        backgroundImage: `url(${montyImages[getRandomMonty]})`,
+      }}
+    >
+      <Message />
+      <Boxes />
+      <Score data={score} />
     </div>
   );
 }
---
+++ src/utils/montyImages.js
@@ -0,0 +1,10 @@
+import monty0 from ".././assets/img/monty_1.jpg";
+import monty1 from ".././assets/img/monty_2.jpg";
+import monty2 from ".././assets/img/monty_3.jpg";
+import monty3 from ".././assets/img/monty_4.jpeg";
+import monty4 from ".././assets/img/monty_5.jpg";
+import monty5 from ".././assets/img/monty_6.jpg";
+
+const montyImages = [monty0, monty1, monty2, monty3, monty4, monty5];
+
+export default montyImages;
